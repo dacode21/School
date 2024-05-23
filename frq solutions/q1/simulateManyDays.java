1946c15ba52f127dd932acb433861f22abb1d087
dacode21 <davidl177@nycstudents.net>
2024-05-22 21:57:16 -0400

    Update simulateManyDays.java

--- frq solutions/q1/simulateManyDays.java
+++ frq solutions/q1/simulateManyDays.java
@@ -1,1 +1,10 @@
-
+  public int simulateManyDays(int numBirds, int numDays){
+    int count = 0;
+    while (currentFood > 0 && numDays > 0){
+      count++;
+      simulateOneDay(numBirds);
+      numDays--;
+    }
+    return count;
+  }
+}
